59e748cd076cbada49f9e6259d1083e913069367
Lisa Susheel <lisasusheel12@gmail.com>
2024-10-07 20:31:49 +0530

    Details section added and CSS alignemnt issues fixed

--- swing_trading/src/components/ChartContainer.js
+++ swing_trading/src/components/ChartContainer.js
@@ -1,39 +1,75 @@
+// import React from "react";
+// import Card from "./Card";
+
+// import Header from "./Header";
+// import Details from "./Details";
+// import { mockCompanyDetails } from "../constants/mock";
+
+
+
+
+// const ChartContainer = () => {
+//   return (
+//     <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 
+//     xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
+      
+//       <div className = "col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
+//        <Header name = {mockCompanyDetails.name}></Header>
+//       </div>
+//         <div className="md:col-span-2 row-span-4">
+//         <Card >
+//             Chart
+//         </Card>
+//         </div>
+//         <div>
+//         <Card>
+//             Overview
+//         </Card>
+//         </div>
+
+//         <div className = "row-span-2 xl:row-span-3">
+//         <Details details ={mockCompanyDetails}>
+
+//         </Details>
+//         </div>  
+//       </div>
+    
+//   )
+// }
+
+// export default ChartContainer
+
+
 import React from "react";
 import Card from "./Card";
-
 import Header from "./Header";
+import Details from "./Details";
 import { mockCompanyDetails } from "../constants/mock";
-
-
-
 
 const ChartContainer = () => {
   return (
-    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 
-    xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
-      
-      <div className = "col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
-       <Header name = {mockCompanyDetails.name}></Header>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 p-10 font-quicksand auto-rows-auto">
+      {/* Header */}
+      <div className="col-span-1 md:col-span-2 xl:col-span-3">
+        <Header name={mockCompanyDetails.name} />
       </div>
-        <div className="md:col-span-2 row-span-4">
-        <Card >
-            Chart
-        </Card>
-        </div>
-        <div>
-        <Card>
-            Overview
-        </Card>
-        </div>
 
-        <div className = "row-span-2 xl:row-span-3">
-        <Card >
-            Details
-        </Card>
-        </div>  
+      {/* Chart */}
+      <div className="col-span-1 md:col-span-2 row-span-4">
+        <Card>Chart</Card>
       </div>
-    
-  )
-}
 
-export default ChartContainer
+      {/* Overview */}
+      <div className="col-span-1">
+        <Card>Overview</Card>
+      </div>
+
+      {/* Details */}
+      <div className="col-span-1 row-span-2 xl:row-span-3">
+        <Details details={mockCompanyDetails} />
+      </div>
+    </div>
+  );
+};
+
+export default ChartContainer;
