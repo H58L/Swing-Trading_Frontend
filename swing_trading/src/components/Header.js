33c80a4145fc4c57d6c8e210a637195f216e1156
Lisa Susheel <lisasusheel12@gmail.com>
2024-10-08 09:51:28 +0530

    Merged Landing page and charts

--- swing_trading/src/components/Header.js
+++ swing_trading/src/components/Header.js
@@ -72,25 +72,5 @@ const Header = ({name}) => {
 
 export default Header;
 
-// import React from 'react'
-// import Search from './Search'
-// import ThemeIcon from './ThemeIcon'
-// import { mockCompanyDetails } from '../constants/mock'
 
-// const Header = ({name}) => {
-//   return (
-//     <>
-//      <div className = "xl:px-32">
-      
-//       <h1 className="text-5xl">{name}</h1>
-//             <Search></Search>
-        
-//     </div>  
-//     <ThemeIcon></ThemeIcon>
-//     </>
-    
-//   )
-// }
-
-// export default Header
 
