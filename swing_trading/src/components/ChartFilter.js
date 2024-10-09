537486a1eb9c2e0b219b12e4daa602fc83224fae
Lisa Susheel <lisasusheel12@gmail.com>
2024-10-09 19:43:52 +0530

    | SCRUM-37 | applied dark mode to chart filters

--- swing_trading/src/components/ChartFilter.js
+++ swing_trading/src/components/ChartFilter.js
@@ -15,15 +15,24 @@
 // export default ChartFilter
 
 import React from 'react';
+import Card from './Card';
+import ThemeContext from '../context/ThemeContex';
+import { useContext } from 'react';
 
 const ChartFilter = ({ text, active, onClick }) => {
+  const {darkMode} = useContext(ThemeContext);
   return (
-    <button 
-      className={`p-2 m-1 rounded ${active ? 'bg-indigo-500 text-white' : 'bg-gray-200 text-black'}`} 
+   <div className= {`${darkMode ? "bg-gray-900 border-gray-800" : "bg-white border-neutral-200 "  }`}>
+       <button 
+      className={`p-2 m-1 rounded ${active ? 'bg-indigo-500 text-white' : 'bg-gray-200 text-black'}` }
       onClick={onClick}
     >
       {text}
     </button>
+   </div>
+   
+    
+    
   );
 };
 
