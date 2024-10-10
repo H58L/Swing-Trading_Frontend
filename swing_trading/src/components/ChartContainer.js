b571ae8e94af47efb3606d77bf7bd0a4bbc2dc90
Lisa Susheel <lisasusheel12@gmail.com>
2024-10-10 09:49:36 +0530

    | SCRUM-37 | applied dark mode trial

--- swing_trading/src/components/ChartContainer.js
+++ swing_trading/src/components/ChartContainer.js
@@ -18,7 +18,7 @@ const ChartContainer = () => {
   return (
     <div
       className={`h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 
-    xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand relative bg-neutral-100 
+    xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand relative
     ${darkMode ? "bg-gray-900 text-gray-300": "bg-neutral-100"}`}
     >
       {/* Removed absolute positioning from ThemeIcon and added relative to container */}
