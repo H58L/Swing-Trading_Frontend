1b008d0c6707896fe44ad537e0c662da11e93026
Lisa Susheel <lisasusheel12@gmail.com>
2024-10-26 21:15:10 +0530

    Updates

--- swing_trading/src/context/ThemeContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const ThemeContext = React.createContext(); //creating new context
-
-export default ThemeContext;
--- swing_trading/src/redux/constants/action-types.js
+++
@@ -1,4 +0,0 @@
-export const ActionTypes = {
-  FETCH_STOCK_DATA: "FETCH_STOCK_DATA", // For successfully fetching stock data
-  FETCH_STOCK_DATA_ERROR: "FETCH_STOCK_DATA_ERROR", // For handling errors
-};
