dfc32ea6db4d8d7b5b720afbbf764b998e60063f
Lisa Susheel <lisasusheel12@gmail.com>
2024-10-28 16:45:27 +0530

    Added Alert Header modified color

--- swing_trading/src/components/AlertHeading.js
+++ swing_trading/src/components/AlertHeading.js
@@ -1,38 +1,3 @@
-// import React from 'react'
-// import ThemeContext from '../context/ThemeContext'
-// import { useContext } from 'react';
-
-
-
-// const AlertHeading = () => {
-
-//     const { darkMode, setDarkMode } = useContext(ThemeContext);
-//   return (
-//     <div className = {`text-gray-100 text-lg font-semibold alert-item mb-3  rounded p-4 shadow-sm border border-gray-100 w-full
-//     ${darkMode ? "bg-gray-900 text-gray-100" : "bg-white"}`}>
-//       Set Alerts
-//     </div>
-//   )
-// }
-
-// export default AlertHeading
-
-
-// import React from 'react';
-// import ThemeContext from '../context/ThemeContext';
-// import { useContext } from 'react';
-
-// const AlertHeading = () => {
-//   const { darkMode } = useContext(ThemeContext);
-
-//   return (
-//     <div className={`text-lg font-bold mb-2 ${darkMode ? 'text-gray-200' : 'text-gray-800'}`}>
-//       Your Alerts
-//     </div>
-//   );
-// };
-
-// export default AlertHeading;
 
 import React from 'react';
 import ThemeContext from '../context/ThemeContext';
@@ -48,8 +13,8 @@ const AlertHeading = () => {
       </div>
       <button
         className={`px-3 py-1 text-sm font-semibold rounded ${
-          darkMode ? 'bg-gray-700 text-gray-200' : 'bg-blue-500 text-white'
-        } hover:${darkMode ? 'bg-gray-600' : 'bg-blue-600'}`}
+          darkMode ? 'bg-indigo-700 text-gray-200 hover:bg-purple-700' : 'bg-gray-900 text-white hover:bg-blue-600'
+        }`}
       >
         Set Alerts
       </button>
@@ -58,3 +23,4 @@ const AlertHeading = () => {
 };
 
 export default AlertHeading;
+
