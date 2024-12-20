df1f9f9a304349d62a344455ac9d5ebf7184f25d
Lisa Susheel <lisasusheel12@gmail.com>
2024-12-20 11:58:03 +0530

    changed URL to railway indicator

--- swing_trading/src/components/IndicatorChart.js
+++ swing_trading/src/components/IndicatorChart.js
@@ -62,7 +62,11 @@ const IndicatorChart = () => {
     try {
       console.log("Selected Indicator: ", selectedIndicator);
 
-      const response = await axios.get("http://localhost:5000/indicators", {
+      // const response = await axios.get("http://localhost:5000/indicators", {
+      //   params: { ticker, indicator: selectedIndicator },
+      // });
+
+      const response = await axios.get("https://swingtrading-production.up.railway.app/indicators", {
         params: { ticker, indicator: selectedIndicator },
       });
 
